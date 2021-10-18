94332812dc7bbd67fcb94f07019c93ae3069a9cb
inshal ayaz <inshal.ayaz14@gmail.com>
2021-10-18 13:29:44 +0500

    Login page complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Login />
     </div>
   );
 }
